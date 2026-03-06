58ba17825294714cc0efaf624b40ded7b6e425b0
Himanshu Karnwal <himanshukar1810@gmail.com>
2026-03-06 12:47:22 +0530

    update deployment script to remove unnecessary docker compose down command; update greeting message to v10

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World v9');
+    res.send('Hello World v10');
 });
 
 const PORT = process.env.PORT || 3000;
